e28d9bb1bb8022a68af321c7149dfb17aac84f9e
Hewitt School Account <school@Hewitts-MacBook-Pro.local>
2022-09-27 14:47:16 -0700

    Added a featuure that finds the contours/facial features of your face. Need to make it work better.

--- cvProject.py
+++ cvProject.py
@@ -4,26 +4,33 @@ import numpy as np
 import cv2
 import os
 
+from PIL import Image, ImageDraw
+import face_recognition
+
+
 # path to the folder with all the images containing faces to idenitfy - name of file = face name
 faces_path = "/Users/school/Documents/School/AHCompSci/opencv/faces"
 
 def get_face_encodings():
     face_names = os.listdir(f"{faces_path}")
     face_encodings = []
+    face_landmarks_list = []
 
     for i, name in enumerate(face_names):
         face = fr.load_image_file(f"{faces_path}//{name}")
         face_encodings.append(fr.face_encodings(face)[0])
-
         face_names[i] = name.split(".")[0]
 
-    return face_encodings, face_names
+        face_landmarks = face_recognition.face_landmarks(face)
+        face_landmarks_list.append(face_landmarks)
+
+    return face_encodings, face_names, face_landmarks_list
 
 
 
 def find_Faces():
 
-    face_encodings, face_names = get_face_encodings()
+    face_encodings, face_names, face_landmarks_list = get_face_encodings()
 
     # store video from latpop camera in variable video
     video = cv2.VideoCapture(0)
@@ -42,6 +49,17 @@ def find_Faces():
         face_locations = fr.face_locations(rgb_image)
         unknown_encodings = fr.face_encodings(rgb_image, face_locations)
 
+#-------------------------------------
+
+
+
+
+
+
+
+
+#-------------------------------------
+
         for face_encoding, face_location in zip(unknown_encodings, face_locations):
 
             result = fr.compare_faces(face_encodings, face_encoding, 0.45)
@@ -57,8 +75,36 @@ def find_Faces():
                 font = cv2.FONT_HERSHEY_DUPLEX
                 cv2.putText(image, name, (left*scl, bottom*scl + 20), font, 0.8, (255, 255, 255), 1)
 
+
+        # start test ------------------------------------------------------------------------
+
+        face_landmarks_list = face_recognition.face_landmarks(image)
+
+        print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
+
+        # Create a PIL imagedraw object so we can draw on the picture
+        pil_image = Image.fromarray(image)
+        d = ImageDraw.Draw(pil_image)
+
+
+        for face_landmarks in face_landmarks_list:
+
+            # Print the location of each facial feature in this image
+            for facial_feature in face_landmarks.keys():
+                print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+
+            # Let's trace out each facial feature in the image with a line!
+            for facial_feature in face_landmarks.keys():
+                d.line(face_landmarks[facial_feature], width=5)
+
+        # Show the picture
+        pil_image.show()
+
+        # end test ------------------------------------------------------------------------
+
         cv2.imshow("frame", image)
         cv2.waitKey(1)
+
 
 
 def find_Words(file):
@@ -85,14 +131,44 @@ def find_Words(file):
     f.close()
 
 
+def test(name):
+
+    # Load the jpg file into a numpy array
+    image = face_recognition.load_image_file("faces/" + name + ".jpg")
+
+    # Find all facial features in all the faces in the image
+    face_landmarks_list = face_recognition.face_landmarks(image)
+
+    print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
+
+    # Create a PIL imagedraw object so we can draw on the picture
+    pil_image = Image.fromarray(image)
+    d = ImageDraw.Draw(pil_image)
+
+    for face_landmarks in face_landmarks_list:
+
+        # Print the location of each facial feature in this image
+        for facial_feature in face_landmarks.keys():
+            print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+
+        # Let's trace out each facial feature in the image with a line!
+        for facial_feature in face_landmarks.keys():
+            d.line(face_landmarks[facial_feature], width=5)
+
+    # Show the picture
+    pil_image.show()
+
 def main():
 
     print("\nWelcome to Hewitt's Open CV Project!!!")
-    file = input("\nPlease input the filename of an image: ")
+    #file = input("\nPlease input the filename of an image: ")
 
     # Use tesseract to get words from an image store into text file
-    find_Words(file) # give name of image
+    #find_Words(file) # give name of image
     print("\nGet your files to find a text file containing all text from the image or look at a preview of it in terminal.") # fix when this prints
+
+    name = input("Please input your name (Firstname_Lastname): ")
+    test(name)
 
     print("\nNow, let's try to figure out who you are.")
     consent = input("Can I figure out who you are? (y/n): ")
@@ -111,3 +187,5 @@ main()
 # ask dr. j how enumerate works *************************************************
 
 # finish comments and read me
+
+# have the data - now just need to loop over the data and draw it on the image
